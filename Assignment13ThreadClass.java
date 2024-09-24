505588674ef7391d09e048a331c90dbf2f4b946d
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-09-24 22:12:50 +0530

    Assignment 13 WAP on thread class (thread.sleep)

---
+++ Assignment13ThreadClass.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class Assignment13ThreadClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	System.out.println("Rohit");
+	Thread.sleep(2000);
+	System.out.println("is");
+	Thread.sleep(2000);
+	System.out.println("good");
+	Thread.sleep(2000);
+	System.out.println("boy");
+	Thread.sleep(2000);
+	System.out.println("End");
+	Thread.sleep(2000);
+	
+	}
+
+}
