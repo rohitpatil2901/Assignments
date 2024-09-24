03f16255fa82f1dfbb613006fb732c4d4975cf8d
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-09-24 22:15:12 +0530

    Assignment15SinglelevelinheritanceNonStaticmethods

---
+++ T1.java
@@ -0,0 +1,29 @@
+package Assignments;
+
+public class T1 {
+	
+	void testCase1(){
+		System.out.print("One");
+	}
+}
+	
+	
+	 class T2 extends T1{
+	
+		 public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				
+		T2 test=new T2();
+		test.testCase1();
+		test.testCase2();
+				
+			}
+			
+		 
+		void testCase2(){
+			System.out.println("Two");
+			
+		}}
+		
+
+
