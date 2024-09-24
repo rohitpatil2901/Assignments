ee3d4fb3d31e1af0bc1c26ffc26e3d46a7f96177
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-09-24 22:14:04 +0530

    Assignment14SinglelevelinheritanceStatic methods

---
+++ One.java
@@ -0,0 +1,25 @@
+package Assignments;
+
+ class One {
+
+	static void first() {
+		
+		System.out.print("First");
+	}
+
+}
+
+ class Two extends One{
+public static void main(String args[]) {
+	
+	first();
+	Second();
+	
+}
+	static void Second() {
+		
+		System.out.print("Second");
+	}
+
+}
+ 
