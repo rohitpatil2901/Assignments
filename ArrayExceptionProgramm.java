34f82ac7d8a3bb314dbd64e1b65312747507d995
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-10 18:55:39 +0530

    Assignment27

---
+++ ArrayExceptionProgramm.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class ArrayExceptionProgramm {
+
+	public static void main(String[] args) {
+		
+		String str="Toyota",str1="";
+		
+		for(int i=str.length()-1;i>=-3;i--) {
+			str1=str1+str.charAt(i);
+			
+		}
+		System.out.println(str1);
+	}
+
+}
