0ad9af2e74972dd17f815e75d6f9999cdfe479d3
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-15 15:22:58 +0530

    Assignment: 40

---
+++ ReplaceStringWithNothing.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class ReplaceStringWithNothing {
+
+	public static void main(String[] args) {
+		
+		String str="kv no 2";
+		
+		String nStr="";
+		nStr=nStr+str.replace("2", "");
+		System.out.println(nStr);
+		
+
+	}
+
+}
