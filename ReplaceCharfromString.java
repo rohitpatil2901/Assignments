a29760bb955158795794ded5251530dee57d4c04
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-15 15:01:46 +0530

    Assignment:38

---
+++ ReplaceCharfromString.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class ReplaceCharfromString {
+
+	public static void main(String[] args) {
+		
+		String str="Kv no 2";
+		String str1 = str.replace('K', 'T');
+		System.out.println(str1);
+		
+
+	}
+
+}
