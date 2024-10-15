3277b0abdd5e5a9854ed733aac9e5e7deebace72
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-15 15:13:17 +0530

    Assignment: 39

---
+++ ReplaceWordFromString.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class ReplaceWordFromString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="I'm in India";
+		String nStr="";
+		nStr=nStr+str.replace("India", "Goa");
+		
+		System.out.println(nStr);
+		
+	}
+
+}
