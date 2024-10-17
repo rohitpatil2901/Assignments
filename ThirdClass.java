d3cb14ea3a9b4d233080b7423fb4f2db83782d97
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 18:13:30 +0530

    Assignment: 57

---
+++ ThirdClass.java
@@ -0,0 +1,19 @@
+package fourAccessSpecifiersj;
+
+import acthree.EpicOne;
+
+public class ThirdClass  {
+
+	public static void main(String[] args) {
+
+
+		EpicOne EO=new EpicOne();
+		System.out.println(EO.e0);// we can access only public variables and methods 
+		//from another package using instance of same class
+		
+		
+		
+
+	}
+
+}
