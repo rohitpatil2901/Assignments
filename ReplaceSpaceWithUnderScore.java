1015e87b6ef7731ad099a77a53e592f87a66abc5
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 09:04:16 +0530

    Assignment: 48

---
+++ ReplaceSpaceWithUnderScore.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+public class ReplaceSpaceWithUnderScore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="My name is Shyam";
+		
+		String result=str.replace(' ','_');
+		
+		System.out.println(result);
+	}
+
+}
