25a29ea31ac32819cfb144ea5662a3b439d783f0
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 08:49:36 +0530

    Assignment: 46

---
+++ FindWordEndsWithY.java
@@ -0,0 +1,20 @@
+package Assignments;
+
+public class FindWordEndsWithY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="I'm a boy";
+		
+		if(str.endsWith("y")) {
+			
+			System.out.println("Word in given string is ends with y");
+		}
+		
+		else {
+			System.out.println("Word in given string is not ends with y");
+		}
+	}
+
+}
