f58c2ae1514f3b43dd015800718f71c7053576e2
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 19:23:06 +0530

    Assignment: 59

---
+++ ConvertStringtoArray.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class ConvertStringtoArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="Rohit";
+		char str1[]=str.toCharArray();
+		
+		System.out.println(str1[0]);
+	}
+
+}
