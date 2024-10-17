f7dad23da8feb6ec531bbced57ddadc6abb99a53
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 08:55:14 +0530

    Assignment: 47

---
+++ FindTheLetterSPresentOrNot.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class FindTheLetterSPresentOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="I'm a imple boy";
+		
+		if(str.contains("s")) {
+			
+			System.out.println("Given string contains s");
+		}
+		
+		else {
+			System.out.println("Given string not contains s");
+		}
+		
+	}
+
+}
