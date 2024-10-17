e2a894507bf7aa203b42b3c66d6ff0d7349ef71a
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 21:36:50 +0530

    Assignment: 62

---
+++ Print1to100WhileLoop.java
@@ -0,0 +1,15 @@
+package assignment2;
+
+public class Print1to100WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=0;
+		while( num<100) {
+			num++;
+			System.out.println(num);
+		}
+		
+	}
+
+}
