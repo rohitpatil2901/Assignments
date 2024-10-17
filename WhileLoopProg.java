9f0212f56bb85a13c0cb519ca45147103c4f348f
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-10-17 21:32:26 +0530

    Assignment 61

---
+++ WhileLoopProg.java
@@ -0,0 +1,15 @@
+package assignment2;
+
+public class WhileLoopProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=10;
+		while(num!=0) {
+			num--;
+			System.out.println(num);
+		}
+	}
+
+}
