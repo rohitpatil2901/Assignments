486f2d453506c1e24637e6f65ff450756ba6b9f3
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-11-16 02:56:29 -0600

    Assignment77 & 78

---
+++ ConvertIntegerToByte.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+
+public class ConvertIntegerToByte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		
+		byte b=(byte)a;// narrowing
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		double d=49345.4334;
+		long l=(long)d; //narrowing
+		System.out.println(l);
+	}
+
+}
