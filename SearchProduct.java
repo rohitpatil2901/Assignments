cedfeb7b76263d302ecbbf905ffee6335ab9a5f8
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-11-25 11:45:28 -0600

    Assignment-106

--- SearchProduct.java
+++ SearchProduct.java
@@ -1,5 +1,8 @@
 package testNgAutomationScenarios;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -9,15 +12,32 @@ public class SearchProduct extends LaunchAndQuitBrowser {
 
 	@Test
 	public void searchAmazonProduct() {
-		
-		
-		WebElement search=driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+
+		WebElement search = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		search.click();
 		search.sendKeys("shoes");
 		search.sendKeys(Keys.ENTER);
+
+//		WebElement firstShoes = driver.findElement(By.xpath("(//div[@class='s-image-padding'])[1]"));
+//		firstShoes.click();
+
 		
-		WebElement firstShoes=driver.findElement(By.xpath("(//div[@class='s-image-padding'])[1]"));
-		firstShoes.click();
+
+		WebElement product = driver.findElement(By.xpath("(//div[@class='s-image-padding'])[1]"));
+		product.click();
+
+		Set<String> windows = driver.getWindowHandles();
+
+		Iterator<String> itr = windows.iterator();
+		String parent = itr.next();
+		String child = itr.next();
+		
+		driver.switchTo().window(child);
+		System.out.println("Child Window Title" + driver.getTitle());
+		
+		WebElement AddToCart=driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+		AddToCart.click();
+
 	}
 
 }
