09c5e080d11ef78de5fa5851f16d59633bd3e14d
rohitpatil2901 <112763750+rohitpatil2901@users.noreply.github.com>
2024-11-25 06:36:25 -0600

    Assignment-94

---
+++ TestTwo.java
@@ -0,0 +1,23 @@
+package TestNGg;
+
+import org.testng.annotations.Test;
+
+public class TestTwo {
+
+	@Test(priority=1)
+	public void Login() {
+		
+		
+	}
+	
+	@Test(priority=2)
+	public void Logout() {
+		
+		
+	}
+	
+	@Test(priority=-1)
+	public void close() {
+		
+	}
+}
